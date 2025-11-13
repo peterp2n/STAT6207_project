81c351989c6a39d04403321aec9caf68037440ba
Tim <tmwong031@gmail.com>
2025-11-13 23:53:09 +0800

    save merged

--- src/stat6207_project/data/preprocessing.py
+++ src/stat6207_project/data/preprocessing.py
@@ -1,6 +1,8 @@
+import numpy as np
 import polars as pl
 from pathlib import Path
 from remove_char import DataCleaner
+from src.stat6207_project.scraper.Data import Data
 
 
 if __name__ == "__main__":
@@ -24,6 +26,26 @@ if __name__ == "__main__":
         output_path=data_folder / 'amazon_cleaned.csv',
         deduplicate_on=['isbn_13']
     )
+
+    arguments = {
+        "db_path": Path("data") / "Topic1_dataset.sqlite",
+        "headless": False,  # Set to True for headless mode
+        "json_folder": Path("data") / "scrapes",
+    }
+    data = Data(arguments)
+    data.load_all_tables()
+    products = data.table_holder.get("products").collect()
+    data_isbns = products["barcode2"].to_list()
+
+    amazon_isbns = amazon_clean["isbn_13"].to_list()
+    api_isbns = api_clean["isbn"].to_list()
+
+    data_amazon_common = np.isin(data_isbns, amazon_isbns)
+    data_api_common = np.isin(data_isbns, api_isbns)
+
+    data_amazon_count, data_amazon_rate = data_amazon_common.sum(), data_amazon_common.mean()
+    data_api_count, data_api_rate = data_api_common.sum(), data_api_common.mean()
+
     # api_clean = cleaner.clean_and_save(
     #     df=api,
     #     output_path=data_folder / 'books_api_cleaned.csv',
@@ -32,10 +54,10 @@ if __name__ == "__main__":
 
     Path(data_folder / "amazon_cleaned.csv").parent.mkdir(parents=True, exist_ok=True)
     Path(data_folder / "api_cleaned.csv").parent.mkdir(parents=True, exist_ok=True)
-    amazon_clean.write_csv(data_folder / "amazon_cleaned.csv", include_header=True)
-    api_clean.write_csv(data_folder / "books_api_cleaned.csv", include_header=True)
+    # amazon_clean.write_csv(data_folder / "amazon_cleaned.csv", include_header=True)
+    # api_clean.write_csv(data_folder / "books_api_cleaned.csv", include_header=True)
 
-    merged = amazon.join(api_clean, how="outer", left_on="isbn_10", right_on="isbn")
+    merged = api_clean.join(amazon, how="left", left_on="isbn", right_on="isbn_10")
     merged.write_csv(data_folder / "merged.csv", include_header=True)
 
 
