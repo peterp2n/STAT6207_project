1c818753f53b00fdaba9e207ed43eee84de08faf
Tim <tmwong031@gmail.com>
2025-11-20 22:06:56 +0800

    Only keep columns >900 non-null except reading_age

--- src/stat6207_project/data/preprocessing_02.py
+++ src/stat6207_project/data/preprocessing_02.py
@@ -1,14 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import polars as pl
 import polars.selectors as cs
+import seaborn as sns
 from pathlib import Path
 
 def load_df(csv_path: str|Path) -> pl.DataFrame:
-    input = pl.scan_csv(csv_path, schema_overrides={
-        "isbn": pl.Utf8,
-        "isbn_10": pl.Utf8,
-        "isbn_13": pl.Utf8,
-        "best_sellers_rank": pl.Int32
-    }).filter(pl.col("scrape_status") == "success")
+    input = pl.scan_csv(csv_path,
+        schema_overrides={
+            "isbn": pl.Utf8,
+            "title": pl.Utf8,
+            "publisher": pl.Utf8,
+            "publication_date": pl.Utf8,
+            "book_format": pl.Utf8,
+            "language": pl.Utf8,
+            "isbn_10": pl.Utf8,
+            "isbn_13": pl.Utf8,
+            "asin": pl.Utf8,
+            "description": pl.Utf8,
+            "product_url": pl.Utf8,
+            "scrape_status": pl.Enum(["success", "fail"]),
+    }
+    ).filter(pl.col("scrape_status") == "success")
     return input
 
 def standardize_columns(lazyframe: pl.LazyFrame) -> pl.LazyFrame:
@@ -36,11 +49,55 @@ def standardize_columns(lazyframe: pl.LazyFrame) -> pl.LazyFrame:
 
 if __name__ == "__main__":
     csv = Path("data") / "merged.csv"
-    df = load_df(csv)
-    df = df.with_columns(
-        pl.col("publication_date").str.to_date(format="%Y-%m-%d", strict=False)
-    )
+    merge = load_df(csv)
+    # Columns:['isbn', 'title', 'publisher', 'publication_date', 'series_name', 'book_format', 'language',
+    # 'print_length', 'isbn_10', 'isbn_13', 'item_weight', 'length', 'width', 'height', 'rating',
+    # 'number_of_reviews', 'availability', 'price', 'best_sellers_rank', 'customer_reviews', 'reading_age',
+    # 'edition', 'author', 'asin', 'description', 'product_url', 'scrape_status']
 
-    numerics = df.select(cs.numeric()).collect()
-    numeric_df = standardize_columns(numerics)
+    with pl.Config() as cfg:
+        cfg.set_tbl_cols(-1)
+        for i in range(0, merge.collect_schema().len(), 5):
+            print(f"\nColumns {i} to {min(i + 5, merge.collect_schema().len())}:")
+            print(merge.select(cs.by_index(range(i, min(i + 5, merge.collect_schema().len())))).describe())
+
+    merge = merge.drop([
+        "asin", "edition", "series_name",
+        "availability",  "product_url",
+        "scrape_status", 'isbn_10', 'isbn_13'])
+    merge2 = merge.select(
+            ['isbn', 'title', 'publisher', 'publication_date',
+             'book_format', "reading_age", 'language', 'print_length',
+             'item_weight', 'length', 'width', 'height',
+             'rating', 'number_of_reviews', 'price',
+             'best_sellers_rank', 'customer_reviews', 'author',
+             'description']
+    ).with_columns(
+            pl.coalesce([
+                pl.col("publication_date").str.to_date(format="%Y-%m-%d", strict=False),
+                pl.col("publication_date").str.to_date(format="%-d/%-m/%Y", strict=False),
+                pl.col("publication_date").str.to_date(format="%d/%m/%Y", strict=False),
+            ]).alias("publication_date"),
+            pl.col("publisher").str.to_lowercase().str.replace_many(
+                [r"\s?ltd\s?", r"[,\s]?inc.?\s?"],
+                ["", ""]
+            ),
+            pl.col("book_format").str.to_lowercase().str.replace_all(r".*kindle.*", "kindle"),
+            pl.col("language").str.to_lowercase(),
+            pl.col("description")
+            .str.replace("\xa0", " ", literal=True)  # literal replacement
+            .str.replace(r"\s?Read more", "", literal=False)
+    ).collect()
+    # Merge2 columns: ['isbn', 'title', 'publisher', 'publication_date', 'book_format', 'reading_age', 'language',
+    # 'print_length', 'item_weight', 'length', 'width', 'height', 'rating', 'number_of_reviews', 'price',
+    # 'best_sellers_rank', 'customer_reviews', 'author', 'description']
+
+    with pl.Config() as cfg:
+        cfg.set_tbl_cols(-1)
+        for i in range(0, merge2.collect_schema().len(), 5):
+            print(f"\nColumns {i} to {min(i + 5, merge2.collect_schema().len())}:")
+            print(merge2.select(cs.by_index(range(i, min(i + 5, merge2.collect_schema().len())))).describe())
+
+    # merge2.write_csv(Path("data") / "merged2.csv", include_bom=True)
+
     pass
